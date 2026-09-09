38feed31a8dbec48cc16f859a8f529220a8d7c36
Jacob <adina.a.jacob@accenture.com>
2026-09-09 17:50:21 +0530

    Updated cd code

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,7 +6,6 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    console.log('REACT_APP_MOVIE_API_URL:', process.env.REACT_APP_MOVIE_API_URL);
     axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
       setMovies(response.data.movies);
     });
